ae4b272e14fa256e2867631b5c70b35506c4ae7d
shameerabdullah.sa7@gmail.com <shameerabdullah.sa7@gmail.com>
2023-12-10 19:50:42 +0500

    Updated app.py and Dockerfiles

--- appointments/app.py
+++ appointments/app.py
@@ -1,27 +1,41 @@
 from flask import Flask, jsonify, request
-app = Flask(__name__)
+from pymongo import MongoClient
+from flask_cors import CORS
 
-appointments = [
-  { 'id': "1",'doctor': "1", 'date': "21 Nov 2023", 'rating':"Good"  },
-  { 'id': "2",'doctor': "1", 'date': "22 Nov 2023", 'rating':"Bad"  },
-  { 'id': "3",'doctor': "2", 'date': "22 Nov 2023", 'rating':"Good"  },
-  { 'id': "4",'doctor': "1", 'date': "22 Nov 2023", 'rating':"Bad"  },
-  { 'id': "5",'doctor': "2", 'date': "22 Nov 2023", 'rating':"Good"  },
+app = Flask(__name__)
+CORS(app)
+client = MongoClient('mongodb://localhost:27017/')
+db = client['AppointmentSystemDB']
+appointments_collection = db['Appointments']
+
+initial_data = [
+    {'id': "A1", 'doctor': "D1", 'date': "01 Jan 2024", 'rating': "Great"},
+    {'id': "A2", 'doctor': "D2", 'date': "02 Jan 2024", 'rating': "Average"},
+    {'id': "A3", 'doctor': "D3", 'date': "03 Jan 2024", 'rating': "Good"},
+    {'id': "A4", 'doctor': "D1", 'date': "04 Jan 2024", 'rating': "Great"},
+    {'id': "A5", 'doctor': "D4", 'date': "05 Jan 2024", 'rating': "Average"}
 ]
 
-@app.route('/hello')
-def hello():
-  greeting = "Hello world!"
-  return greeting
+if appointments_collection.count_documents({}) == 0:
+    appointments_collection.insert_many(initial_data)
+
+@app.route('/welcome')
+def welcome():
+    greeting = "Welcome to the Appointment System!"
+    return greeting
 
 @app.route('/appointments', methods=["GET"])
-def getAppointments():
-  return jsonify(appointments)
+def list_appointments():
+    appointments_list = list(appointments_collection.find({}, {'_id': False}))
+    return jsonify(appointments_list)
 
-@app.route('/appointment/<id>', methods=["GET"])
-def getAppointment(id):
-  id = int(id) - 1
-  return jsonify(appointments[id])
+@app.route('/appointment/<appointment_id>', methods=["GET"])
+def get_specific_appointment(appointment_id):
+    appointment = appointments_collection.find_one({'id': appointment_id}, {'_id': False})
+    if appointment:
+        return jsonify(appointment)
+    else:
+        return jsonify({'error': 'Appointment not found'}), 404
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0",port=7070)
+    app.run(host="0.0.0.0", port=8080)
--- doctors/app.py
+++ doctors/app.py
@@ -1,24 +1,41 @@
 from flask import Flask, jsonify, request
-app = Flask(__name__)
+from pymongo import MongoClient
+from flask_cors import CORS
 
-doctors = [
-  { 'id': "1",'firstName': "Muhammad Ali", 'lastName': "Kahoot", 'speciality':"DevOps"  },
-  { 'id': "2",'firstName': "Good", 'lastName': "Doctor",'speciality':"Test"  }
+app = Flask(__name__)
+CORS(app)
+client = MongoClient('mongodb://localhost:27017/')
+db = client['healthcare_db']
+doctors_collection = db['healthcare_professionals']
+
+initial_data = [
+    {'id': "D1", 'firstName': "John", 'lastName': "Smith", 'speciality': "Pediatrics"},
+    {'id': "D2", 'firstName': "Jane", 'lastName': "Doe", 'speciality': "Orthopedics"},
+    {'id': "D3", 'firstName': "Emily", 'lastName': "Johnson", 'speciality': "Oncology"},
+    {'id': "D4", 'firstName': "Michael", 'lastName': "Brown", 'speciality': "Neurology"},
+    {'id': "D5", 'firstName': "Sophia", 'lastName': "Wilson", 'speciality': "Dentistry"}
 ]
 
-@app.route('/hello')
-def hello():
-  greeting = "Hello world!"
-  return greeting
+if doctors_collection.count_documents({}) == 0:
+    doctors_collection.insert_many(initial_data)
 
-@app.route('/doctors', methods=["GET"])
-def getDoctors():
-  return jsonify(doctors)
+@app.route('/welcome')
+def welcome():
+    greeting = "Welcome to the Healthcare System!"
+    return greeting
 
-@app.route('/doctor/<id>', methods=["GET"])
-def getDoctor(id):
-  id = int(id) - 1
-  return jsonify(doctors[id])
+@app.route('/professionals', methods=["GET"])
+def list_professionals():
+    professionals_list = list(doctors_collection.find({}, {'_id': False}))
+    return jsonify(professionals_list)
+
+@app.route('/professional/<professional_id>', methods=["GET"])
+def get_specific_professional(professional_id):
+    professional = doctors_collection.find_one({'id': professional_id}, {'_id': False})
+    if professional:
+        return jsonify(professional)
+    else:
+        return jsonify({'error': 'Professional not found'}), 404
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0",port=9090)
+    app.run(host="0.0.0.0", port=8081)
