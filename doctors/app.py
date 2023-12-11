f0ae99f34ae762a6adf26bd33627e47a3ebb5bf5
shameerabdullah.sa7@gmail.com <shameerabdullah.sa7@gmail.com>
2023-12-11 13:30:48 +0500

    Detaching DB

--- doctors/app.py
+++ doctors/app.py
@@ -1,41 +1,27 @@
 from flask import Flask, jsonify, request
-from pymongo import MongoClient
-from flask_cors import CORS
-
 app = Flask(__name__)
-CORS(app)
-client = MongoClient('mongodb://localhost:27017/')
-db = client['healthcare_db']
-doctors_collection = db['healthcare_professionals']
 
-initial_data = [
-    {'id': "D1", 'firstName': "John", 'lastName': "Smith", 'speciality': "Pediatrics"},
-    {'id': "D2", 'firstName': "Jane", 'lastName': "Doe", 'speciality': "Orthopedics"},
-    {'id': "D3", 'firstName': "Emily", 'lastName': "Johnson", 'speciality': "Oncology"},
-    {'id': "D4", 'firstName': "Michael", 'lastName': "Brown", 'speciality': "Neurology"},
-    {'id': "D5", 'firstName': "Sophia", 'lastName': "Wilson", 'speciality': "Dentistry"}
+doctors = [
+    {'id': "1", 'firstName': "Muhammad Ali", 'lastName': "Kahoot", 'speciality': "DevOps"},
+    {'id': "2", 'firstName': "Good", 'lastName': "Doctor", 'speciality': "Test"},
+    {'id': "3", 'firstName': "John", 'lastName': "Smith", 'speciality': "Pediatrics"},
+    {'id': "4", 'firstName': "Jane", 'lastName': "Doe", 'speciality': "Orthopedics"},
+    {'id': "5", 'firstName': "Emily", 'lastName': "Johnson", 'speciality': "Oncology"}
 ]
 
-if doctors_collection.count_documents({}) == 0:
-    doctors_collection.insert_many(initial_data)
-
-@app.route('/welcome')
-def welcome():
-    greeting = "Welcome to the Healthcare System!"
+@app.route('/hello')
+def hello():
+    greeting = "Hello world!"
     return greeting
 
-@app.route('/professionals', methods=["GET"])
-def list_professionals():
-    professionals_list = list(doctors_collection.find({}, {'_id': False}))
-    return jsonify(professionals_list)
+@app.route('/doctors', methods=["GET"])
+def getDoctors():
+    return jsonify(doctors)
 
-@app.route('/professional/<professional_id>', methods=["GET"])
-def get_specific_professional(professional_id):
-    professional = doctors_collection.find_one({'id': professional_id}, {'_id': False})
-    if professional:
-        return jsonify(professional)
-    else:
-        return jsonify({'error': 'Professional not found'}), 404
+@app.route('/doctor/<id>', methods=["GET"])
+def getDoctor(id):
+    id = int(id) - 1
+    return jsonify(doctors[id])
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8081)
+    app.run(host="0.0.0.0", port=9090)
