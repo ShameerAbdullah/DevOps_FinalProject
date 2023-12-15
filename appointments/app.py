5b5206e4f64d1a9e42547c4bc1d3728ce6f3bb63
shameerabdullah.sa7@gmail.com <shameerabdullah.sa7@gmail.com>
2023-12-15 15:03:05 +0500

    Updates

--- appointments/app.py
+++ appointments/app.py
@@ -1,28 +1,59 @@
 from flask import Flask, jsonify, request
-app = Flask(__name__)
+from pymongo import MongoClient, errors
+from flask_cors import CORS
+import os
 
-appointments = [
-    {'id': "1", 'doctor': "1", 'date': "21 Nov 2023", 'rating': "Good"},
-    {'id': "2", 'doctor': "1", 'date': "22 Nov 2023", 'rating': "Bad"},
-    {'id': "3", 'doctor': "2", 'date': "22 Nov 2023", 'rating': "Good"},
-    {'id': "4", 'doctor': "1", 'date': "22 Nov 2023", 'rating': "Bad"},
-    {'id': "5", 'doctor': "2", 'date': "22 Nov 2023", 'rating': "Good"}
+app = Flask(__name__)
+CORS(app)
+
+# Using environment variables for database URI
+mongo_uri = os.getenv('MONGO_URI', 'mongodb://localhost:27017/')
+try:
+    client = MongoClient(mongo_uri)
+    db = client['medical_center_db']
+    medical_appointments_collection = db['medical_appointments']
+except errors.ConnectionFailure:
+    print("Could not connect to MongoDB")
+
+initial_medical_appointments = [
+    {'id': "1", 'patient': "John Doe", 'date': "21 Nov 2023", 'status': "Confirmed"},
+    {'id': "2", 'patient': "Jane Smith", 'date': "22 Nov 2023", 'status': "Cancelled"},
+    {'id': "3", 'patient': "Emily Johnson", 'date': "23 Nov 2023", 'status': "Pending"},
+    {'id': "4", 'patient': "Michael Brown", 'date': "24 Nov 2023", 'status': "Confirmed"},
+    {'id': "5", 'patient': "Linda Davis", 'date': "25 Nov 2023", 'status': "Rescheduled"},
+    {'id': "6", 'patient': "Robert Wilson", 'date': "26 Nov 2023", 'status': "Confirmed"},
+    {'id': "7", 'patient': "Maria Garcia", 'date': "27 Nov 2023", 'status': "No-show"},
+    {'id': "8", 'patient': "James Miller", 'date': "28 Nov 2023", 'status': "Completed"},
+    {'id': "9", 'patient': "Patricia Anderson", 'date': "29 Nov 2023", 'status': "Pending"},
+    {'id': "10", 'patient': "Charles Thomas", 'date': "30 Nov 2023", 'status': "Cancelled"},
 ]
 
+# Insert initial values if the collection is empty
+if medical_appointments_collection.count_documents({}) == 0:
+    medical_appointments_collection.insert_many(initial_medical_appointments)
 
 @app.route('/hello')
 def hello():
-    greeting = "Hello world!"
-    return greeting
+    return "Hello from Medical Center API!"
 
-@app.route('/appointments', methods=["GET"])
-def getAppointments():
-    return jsonify(appointments)
+@app.route('/medical_appointments', methods=["GET"])
+def get_medical_appointments():
+    appointments_list = list(medical_appointments_collection.find({}, {'_id': False}))
+    return jsonify(appointments_list)
 
-@app.route('/appointment/<id>', methods=["GET"])
-def getAppointment(id):
-    id = int(id) - 1
-    return jsonify(appointments[id])
+@app.route('/medical_appointment/<appointment_id>', methods=["GET"])
+def get_medical_appointment(appointment_id):
+    appointment = medical_appointments_collection.find_one({'id': appointment_id}, {'_id': False})
+    if appointment:
+        return jsonify(appointment)
+    else:
+        return jsonify({'error': 'Appointment not found'}), 404
+
+@app.route('/medical_appointment', methods=["POST"])
+def create_medical_appointment():
+    appointment_data = request.json
+    medical_appointments_collection.insert_one(appointment_data)
+    return jsonify({'message': 'Appointment created successfully'}), 201
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8082)
--- doctors/app.py
+++ doctors/app.py
@@ -1,28 +1,59 @@
-from flask import Flask, jsonify
-app = Flask(__name__)
+from flask import Flask, jsonify, request
+from pymongo import MongoClient, errors
+from flask_cors import CORS
+import os
 
-doctors = [
-    {'id': "1", 'firstName': "Muhammad Ali", 'lastName': "Kahoot", 'speciality': "DevOps"},
-    {'id': "2", 'firstName': "Good", 'lastName': "Doctor", 'speciality': "Test"},
-    {'id': "3", 'firstName': "John", 'lastName': "Smith", 'speciality': "Pediatrics"},
-    {'id': "4", 'firstName': "Jane", 'lastName': "Doe", 'speciality': "Orthopedics"},
-    {'id': "5", 'firstName': "Emily", 'lastName': "Johnson", 'speciality': "Oncology"}
+app = Flask(__name__)
+CORS(app)
+
+# Using environment variables for database URI
+mongo_uri = os.getenv('MONGO_URI', 'mongodb://localhost:27017/')
+try:
+    client = MongoClient(mongo_uri)
+    db = client['healthcare_db']
+    doctors_collection = db['healthcare_professionals']
+except errors.ConnectionFailure:
+    print("Could not connect to MongoDB")
+
+initial_healthcare_professionals = [
+    {'id': "1", 'firstName': "John", 'lastName': "Doe", 'speciality': "Cardiology"},
+    {'id': "2", 'firstName': "Jane", 'lastName': "Smith", 'speciality': "Neurology"},
+    {'id': "3", 'firstName': "Emily", 'lastName': "Johnson", 'speciality': "Dermatology"},
+    {'id': "4", 'firstName': "Michael", 'lastName': "Brown", 'speciality': "Pediatrics"},
+    {'id': "5", 'firstName': "Linda", 'lastName': "Davis", 'speciality': "General Practice"},
+    {'id': "6", 'firstName': "Robert", 'lastName': "Wilson", 'speciality': "Orthopedics"},
+    {'id': "7", 'firstName': "Maria", 'lastName': "Garcia", 'speciality': "Gynecology"},
+    {'id': "8", 'firstName': "James", 'lastName': "Miller", 'speciality': "Oncology"},
+    {'id': "9", 'firstName': "Patricia", 'lastName': "Anderson", 'speciality': "Psychiatry"},
+    {'id': "10", 'firstName': "Charles", 'lastName': "Thomas", 'speciality': "Anesthesiology"},
+    {'id': "11", 'firstName': "Lisa", 'lastName': "White", 'speciality': "Rheumatology"},
+    {'id': "12", 'firstName': "Paul", 'lastName': "Harris", 'speciality': "Ophthalmology"},
+    {'id': "13", 'firstName': "Nancy", 'lastName': "Clark", 'speciality': "Endocrinology"},
+    {'id': "14", 'firstName': "Kevin", 'lastName': "Lewis", 'speciality': "Urology"},
+    {'id': "15", 'firstName': "Susan", 'lastName': "Lee", 'speciality': "Gastroenterology"},
 ]
 
-@app.route('/hellos')
+
+# Insert initial values if the collection is empty
+if doctors_collection.count_documents({}) == 0:
+    doctors_collection.insert_many(initial_healthcare_professionals)
+
+@app.route('/hello')
 def hello():
-    greeting = "Hello world!"
-    return greeting
+    return "Hello from Healthcare Professionals API!"
 
-@app.route('/doctors', methods=["GET"])
-def getDoctors():
-    return jsonify(doctors)
+@app.route('/healthcare_professionals', methods=["GET"])
+def get_healthcare_professionals():
+    professionals_list = list(doctors_collection.find({}, {'_id': False}))
+    return jsonify(professionals_list)
 
-@app.route('/doctor/<id>', methods=["GET"])
-def getDoctor(id):
-    id = int(id) - 1
-    return jsonify(doctors[id])
+@app.route('/healthcare_professional/<professional_id>', methods=["GET"])
+def get_healthcare_professional(professional_id):
+    professional = doctors_collection.find_one({'id': professional_id}, {'_id': False})
+    if professional:
+        return jsonify(professional)
+    else:
+        return jsonify({'error': 'Healthcare professional not found'}), 404
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8081)
-
